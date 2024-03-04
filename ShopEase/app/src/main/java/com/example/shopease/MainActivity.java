e5971c9d43b6f8fb00d9ddd87ad0fbe152a6545a
Shivaram Reddy <shivaramreddy.91@gmail.com>
2024-03-04 16:15:25 -0600

    Added code for MainActivity(Shivaram Reddy Palla)
    added setOnClickListener  for both login and create buttons which will redirect to login and create account activity

--- ShopEase/app/src/main/java/com/example/shopease/MainActivity.java
+++ ShopEase/app/src/main/java/com/example/shopease/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.shopease;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginBTN);
+        Button createButton = findViewById(R.id.createBTN);
+
+        // Set click listener for login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to login activity
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        // Set click listener for create button
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to create account activity
+                Intent createAccountIntent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(createAccountIntent);
+            }
+        });
     }
 }
