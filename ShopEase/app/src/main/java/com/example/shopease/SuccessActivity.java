12510c37e78aeb89d11c8d7d590aa3c3759f05c6
lakshmipriya222 <143149996+lakshmipriya222@users.noreply.github.com>
2024-03-04 20:18:26 -0600

    Added code for Success Activity

--- ShopEase/app/src/main/java/com/example/shopease/SuccessActivity.java
+++ ShopEase/app/src/main/java/com/example/shopease/SuccessActivity.java
@@ -2,7 +2,10 @@ package com.example.shopease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccessActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
-    }
+
+        Button payNowButton = findViewById(R.id.buttonContinueShopping);
+
+        // Set click listener for the Pay Now button
+        payNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click, navigate to the CheckpageActivity
+                Intent checkpageIntent = new Intent(SuccessActivity.this, ProductListActivity.class);
+                startActivity(checkpageIntent);
+            }
+   });
+}
 }
