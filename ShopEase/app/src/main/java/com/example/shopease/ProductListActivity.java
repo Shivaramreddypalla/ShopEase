a80d0e687f8e2e26ef3160996bbdd009af792e53
malaya sugandhini <142928309+S566637@users.noreply.github.com>
2024-03-04 19:13:15 -0600

    added code for productactivity(Sugandhini)

--- ShopEase/app/src/main/java/com/example/shopease/ProductListActivity.java
+++ ShopEase/app/src/main/java/com/example/shopease/ProductListActivity.java
@@ -2,7 +2,10 @@ package com.example.shopease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductListActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class ProductListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        Button myCartButton = findViewById(R.id.myCartBTN);
+
+        // Set click listener for the MY Cart button
+        myCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click, navigate to the MyCartActivity
+                Intent myCartIntent = new Intent(ProductListActivity.this, ShoppingCartActivity.class);
+                startActivity(myCartIntent);
+            }
+        });
     }
+
 }
