6101a2ce445c4814e012650505061a81b3558333
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-03-04 20:03:09 -0600

    [Anusha Gurijala] - Added code for checkOutActivity
    Implemented functionality for payNow button.

--- ShopEase/app/src/main/java/com/example/shopease/CheckoutActivity.java
+++ ShopEase/app/src/main/java/com/example/shopease/CheckoutActivity.java
@@ -2,7 +2,10 @@ package com.example.shopease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        Button payNowButton = findViewById(R.id.buttonPay);
+
+        // Set click listener for the Pay Now button
+        payNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click, navigate to the CheckpageActivity
+                Intent checkpageIntent = new Intent(CheckoutActivity.this, SuccessActivity.class);
+                startActivity(checkpageIntent);
+            }
+        });
     }
 }
