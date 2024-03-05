13ce6d86f134dfeb03f7ca9de4a20671405edb2b
malaya sugandhini <142928309+S566637@users.noreply.github.com>
2024-03-04 19:17:01 -0600

    Created Shopping cart(Sugandhini)

--- ShopEase/app/src/main/java/com/example/shopease/ShoppingCartActivity.java
+++ ShopEase/app/src/main/java/com/example/shopease/ShoppingCartActivity.java
@@ -2,7 +2,10 @@ package com.example.shopease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class ShoppingCartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
+
+        Button payNowButton = findViewById(R.id.checkoutBTN);
+
+        // Set click listener for the Pay Now button
+        payNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle the click, navigate to the CheckpageActivity
+                Intent checkpageIntent = new Intent(ShoppingCartActivity.this, CheckoutActivity.class);
+                startActivity(checkpageIntent);
+            }
+        });
     }
 }
