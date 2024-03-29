5a28db76d4154f6a376793bff472e7fb5e036f07
malaya sugandhini <142928309+S566637@users.noreply.github.com>
2024-03-28 21:46:32 -0500

    Added shopping adapter(Sugandhini)

---
+++ ShopEase/app/src/main/java/com/example/shopease/ShoppingAdapter.java
@@ -0,0 +1,42 @@
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.
+        NonNull
+        ;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+public class ShoppingAdapter extends RecyclerView.Adapter<ShoppingAdapter.ProductViewHolder> {
+    private List<Product> productList;
+    private Context context;
+    public ShoppingAdapter(List<Product> productList, Context context) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(
+            @NonNull
+            ViewGroup parent, int viewType) {
+        View view = LayoutInflater.
+                from
+                        (parent.getContext()).inflate(R.layout.
+                                shoppingitem_product
+                        , parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull
+            ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.productName.setText(product.getName());
+        holder.productPrice.setText(String.
+                valueOf
+                        (product.getPrice()));
+    }
