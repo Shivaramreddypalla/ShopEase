7321e92ed2e043a953ce28fa149acac7e13c5151
malaya sugandhini <142928309+S566637@users.noreply.github.com>
2024-04-12 12:00:23 -0500

    updated shoppingProduct(sugandhini)

--- ShopEase/app/src/main/java/com/example/shopease/ShoppingProduct.java
+++ ShopEase/app/src/main/java/com/example/shopease/ShoppingProduct.java
@@ -1,4 +1,18 @@
 package com.example.shopease;
 
 public class ShoppingProduct {
+    private String name;
+    private double price;
+
+    public ShoppingProduct(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
