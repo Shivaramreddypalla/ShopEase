64b6257bff8ed7c9626d07aefc7f4e134e3521a3
malaya sugandhini <142928309+S566637@users.noreply.github.com>
2024-04-12 12:01:21 -0500

    updated success activity(Sugandhini)

--- ShopEase/app/src/main/java/com/example/shopease/SuccessActivity.java
+++ ShopEase/app/src/main/java/com/example/shopease/SuccessActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SuccessActivity extends AppCompatActivity {
 
@@ -13,7 +14,18 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+        Intent intent = getIntent();
+        String orderID = intent.getStringExtra("orderID");
 
+        double grandTotal = intent.getDoubleExtra("grandTotal", 0.0); // Default value 0.0 if not found
+        String formattedGrandTotal = String.format("%.2f", grandTotal);
+        // Display order ID and grand total in TextViews
+
+        TextView orderIdTextView = findViewById(R.id.textViewOrderID);
+        orderIdTextView.setText("Order ID: #" + orderID);
+
+        TextView grandTotalTextView = findViewById(R.id.textViewGrandTotal);
+        grandTotalTextView.setText("Grand Total: $" + formattedGrandTotal);
         Button payNowButton = findViewById(R.id.buttonContinueShopping);
 
         // Set click listener for the Pay Now button
@@ -24,6 +36,6 @@ public class SuccessActivity extends AppCompatActivity {
                 Intent checkpageIntent = new Intent(SuccessActivity.this, ProductListActivity.class);
                 startActivity(checkpageIntent);
             }
-   });
-}
+        });
+    }
 }
